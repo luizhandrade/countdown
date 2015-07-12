def3763e37fc38af575776619c479fd6edcac71e
DeAndradeLuiz <luizlhandrade@gmail.com>
2015-07-12 20:32:37 +0200

    Start user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,24 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8001')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_view_countdown_clock(self):
+# Jacq goes to the countdown site
+        self.browser.get('http://localhost:8001')
+
+# She notice the title confirming this is the right site.
+        self.assertIn('Countdown', self.browser.title)
+        self.fail('Finished the test!')
+        
+# She sees this is a countdown to a specific date "september"
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
